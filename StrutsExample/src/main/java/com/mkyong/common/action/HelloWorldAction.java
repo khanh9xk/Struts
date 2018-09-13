64b5edd0ae94a333a1adec1856f7afaa1fbaacae
KhanhND25 <khanh9xkk@gmail.com>
2018-09-13 18:11:18 +0700

    linh
    add fresher
    
    Co-Authored-By: khanhtest1 <huyket1999@gmail.com>

--- StrutsExample/src/main/java/com/mkyong/common/action/HelloWorldAction.java
+++ StrutsExample/src/main/java/com/mkyong/common/action/HelloWorldAction.java
@@ -17,6 +17,7 @@ public class HelloWorldAction extends Action{
 		asdasdasd
 		HelloWorldForm helloWorldForm = (HelloWorldForm) form;
 		helloWorldForm.setMessage("Hello World! Struts");
+		//hello khanh
 		
 		return mapping.findForward("success");
 	}
